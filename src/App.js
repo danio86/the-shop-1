67bd331a3e21fff273fd70e640d80a596d523985
danio86 <>
2023-06-13 10:55:53 +0000

    add navbar and css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-// import logo from './logo.svg';
-import Button from "react-bootstrap/Button";
-import "./App.css";
+import styles from "./App.module.css";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-      <Button variant="primary">Primary</Button>
+    <div className={styles.App}>
+      <NavBar />
     </div>
   );
 }
---
+++ src/components/SideBar.js
@@ -0,0 +1,12 @@
+import React from 'react' 
+import 'bootstrap/dist/css/bootstrap.min.css/'
+// import 'bootstrap-icons/font/bootstrap-icons.js'
+
+
+const SideBar = () => {
+  return (
+    <div className='container-fluid'>SideBar</div>
+  )
+}
+
+export default SideBar
