cae1c035a233dc795b7e86e8a9cafc2917125901
danio86 <>
2023-06-13 14:41:22 +0000

    add limk and pages to navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
+import { Route, Switch } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+            <Route exact path="/" render={() => <h1>Home page</h1>} />
+            <Route exact path="/signin" render={() => <h1>Sign In</h1>} />
+            <Route exact path="/signup" render={() => <h1>Sign Up</h1>} />
+        </Switch>
+      </Container>
     </div>
   );
 }
