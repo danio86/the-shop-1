b62ffe5dd3ba6a4cf964324f985c32196f07ac30
danio86 <>
2023-06-14 11:53:56 +0000

    add image to signin

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import "./api/axiosDefaults"
 import SignUpForm from "./pages/auth/SignUpForm";
+import SignInForm from "./pages/auth/SignInForm";
 
 
 function App() {
@@ -13,8 +14,8 @@ function App() {
       <Container className={styles.Main}>
         <Switch>
             <Route exact path="/" render={() => <h1>Home page</h1>} />
-            <Route exact path="/signin" render={() => <h1>Sign In</h1>} />
             <Route exact path="/signup" render={() => <SignUpForm />} />
+            <Route exact path="/signin" render={() => <SignInForm />} />
         </Switch>
       </Container>
     </div>
