0d75dbd6aa57b2a3cc822a031fdc3d8ce58a7a23
danio86 <>
2023-06-16 11:39:26 +0000

    started toggle navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useRef, useState} from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.png";
 import styles from "../styles/NavBar.module.css";
@@ -10,6 +10,15 @@ import axios from "axios";
 const NavBar = () => {
   const currentUser = useCurrentUser();
   const setCurrentUser = useSetCurrentUser();
+
+  const [expanded, setExpanded] = useState(false);
+//   const { expanded, setExpanded, ref } = useClickOutsideToggle();
+//   hat der user auf user auf den burger geklickt oder nicht
+  const ref = useRef(null);
+//   nur ein Referenzwert
+  useEffect(() => {
+
+  })
 
   const handleSignOut = async () => {
     try {
@@ -81,7 +90,7 @@ const NavBar = () => {
   );
 
   return (
-    <Navbar className={styles.NavBar} expand="md" fixed="top">
+    <Navbar expanded={expanded} className={styles.NavBar} expand="md" fixed="top">
       <Container>
         <NavLink to="/">
           <Navbar.Brand>
@@ -92,7 +101,12 @@ const NavBar = () => {
         {currentUser && addPostIcon}
         {/* shows add link only when user is loggedin */}
 
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle 
+            ref = {ref}
+            // einfach ein referenzattribut (mit wert ref > wurde oben definert)
+            onClick={() => setExpanded(!expanded)}
+            // setExpandet bekommt den gegenteiligen Wert
+            aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
             <NavLink
