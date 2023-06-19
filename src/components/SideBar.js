6d14f450614ee469212292687ce8e5eb441d644d
danio86 <>
2023-06-19 19:26:21 +0000

    add link to sidebar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,6 +1,7 @@
 import React from 'react' 
 import 'bootstrap/dist/css/bootstrap.min.css/'
 // import 'bootstrap-icons/font/bootstrap-icons.js'
+import { NavLink } from 'react-router-dom';
 
 
 const SideBar = () => {
@@ -8,22 +9,41 @@ const SideBar = () => {
         {
             path: "/products",
             name: "products",
-            icon: ""
+            icon: "test1"
         },
         {
             path: "/properties",
             name: "properties",
-            icon: ""
+            icon: "test2"
         },
         {
             path: "/about",
             name: "about",
-            icon: ""
+            icon: "test3"
         },
 
     ]
   return (
-    <div className='container-fluid'>SideBar</div>
+    <div className='container'>
+        <div className='sidebar'>
+            <div className='top-section'>
+                <h1>Logo</h1>
+                <div className='arrow'>
+                    <h1>Pfeil</h1>
+                </div>
+                {
+                   item.map((item, index)=>(
+                       <NavLink to={item.path} key={index} className="link" activeclassName="active">
+                           <div className="icon">{item.icon}</div>
+                           {/* <div style={{display: isOpen ? "block" : "none"}} className="link_text">{item.name}</div> */}
+                       </NavLink>
+                   ))
+               }
+
+            </div>
+        </div>
+        
+    </div>
   )
 }
 
