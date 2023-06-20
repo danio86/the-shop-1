2725481e10d8612ca12c9182c34d07237550d17a
danio86 <>
2023-06-20 12:30:33 +0000

    styled sidebar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,4 +1,4 @@
-import React from 'react' 
+import React, { useState } from 'react' 
 import 'bootstrap/dist/css/bootstrap.min.css/'
 // import 'bootstrap-icons/font/bootstrap-icons.js'
 import { NavLink } from 'react-router-dom';
@@ -8,40 +8,64 @@ import styles from "../styles/SideBar.module.css";
 
 // const SideBar = ({children}) => {
 const SideBar = () => {
+    const [ expanded, setExpanded] = useState(false);
+    // const { expanded, setExpanded, ref } = useClickOutsideToggle();
+    const toggle = () => setExpanded (!expanded);
+
     const item = [
         {
             path: "/products",
             name: "products",
-            icon: <i class="fas fa-store"></i>
+            icon: <i class="fas fa-store"></i>,
+            text: "Products",
         },
         {
             path: "/properties",
             name: "properties",
-            icon: <i class="fas fa-house"></i>
+            icon: <i class="fas fa-house"></i>,
+            text: "Properties",
         },
         {
             path: "/about",
             name: "about",
-            icon: <i class="fas fa-info-circle"></i>
+            icon: <i class="fas fa-info-circle"></i>,
+            text: "About",
         },
 
     ]
   return (
     <div className={styles.Container}>
-        <div className={styles.SideBar}>
-            <div className='top-section'>
-                {/* <h1>Logo</h1> */}
-                <div className='styles.bars'>
-                    <i class="fas fa-hamburger"></i>
-                </div>
+        <div className={styles.SideBar} style={{width: expanded ? "200px" : "50px"}}>
+            <div className={styles.TopSection}>
+            {/* <h1 style={{display: expanded ? "block" : "none"}} className="logo">Logo</h1> */}
+                {/* <div 
+                    className={styles.Hamburger}
+                    onClick={toggle}>
+                    {expanded ? <i class="fas fa-arrow-circle-left"></i> : <i class="fas fa-arrow-circle-right"></i>}
+                </div> */}
                 {
                    item.map((item, index)=>(
-                       <NavLink to={item.path} key={index} className="link" activeclassName="active">
-                           <div className="icon">{item.icon}</div>
+                       <NavLink 
+                            to={item.path} 
+                            key={index} 
+                            className={styles.Link} 
+                            activeclassName="active">
+                           <div 
+                                // style={{marginLeft: expanded ? "50px" : "0px"}}
+                                className={styles.Icon}>{item.icon} 
+                                <h4 
+                                    style={{display: expanded ? "block" : "none"}}
+                                    className={styles.Text}>{item.text}</h4>
+                            </div>
                            {/* <div style={{display: isOpen ? "block" : "none"}} className="link_text">{item.name}</div> */}
                        </NavLink>
                    ))
                }
+               <div 
+                    className={styles.Hamburger}
+                    onClick={toggle}>
+                    {expanded ? <i class="fas fa-arrow-circle-left"></i> : <i class="fas fa-arrow-circle-right"></i>}
+                </div>
 
             </div>
         </div>
