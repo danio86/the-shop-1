8117e891e5cb36b8e73414613f1a2af8a02f061a
danio86 <>
2023-06-20 08:34:44 +0000

    add style to sideBar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -2,34 +2,37 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css/'
 // import 'bootstrap-icons/font/bootstrap-icons.js'
 import { NavLink } from 'react-router-dom';
+import { Navbar, Container, Nav } from "react-bootstrap";
+import styles from "../styles/SideBar.module.css";
 
 
+// const SideBar = ({children}) => {
 const SideBar = () => {
     const item = [
         {
             path: "/products",
             name: "products",
-            icon: "test1"
+            icon: <i class="fas fa-store"></i>
         },
         {
             path: "/properties",
             name: "properties",
-            icon: "test2"
+            icon: <i class="fas fa-house"></i>
         },
         {
             path: "/about",
             name: "about",
-            icon: "test3"
+            icon: <i class="fas fa-info-circle"></i>
         },
 
     ]
   return (
-    <div className='container'>
-        <div className='sidebar'>
+    <div className={styles.Container}>
+        <div className={styles.SideBar}>
             <div className='top-section'>
-                <h1>Logo</h1>
-                <div className='arrow'>
-                    <h1>Pfeil</h1>
+                {/* <h1>Logo</h1> */}
+                <div className='styles.bars'>
+                    <i class="fas fa-hamburger"></i>
                 </div>
                 {
                    item.map((item, index)=>(
@@ -42,7 +45,7 @@ const SideBar = () => {
 
             </div>
         </div>
-        
+        {/* <main>{children}</main> */}
     </div>
   )
 }
