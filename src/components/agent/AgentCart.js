ff9152d884a0a0766afbc9c563108f8594829003
danio86 <>
2023-06-20 16:41:16 +0000

    add empty component files

---
+++ src/components/agent/AgentCart.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AgentCart = () => {
+  return (
+    <div>AgentCart</div>
+  )
+}
+
+export default AgentCart
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { useList } from "@corets/use-list"
-
+import PieChart from '../components/charts/PieChart'
+import TotalRevenue from '../components/charts/TotalRevenue'
+import AgentCart from '../components/agent/AgentCart'
+import PropertyReferrals from '../components/charts/PropertyReferrals'
 
 
 const Homepage = () => {
