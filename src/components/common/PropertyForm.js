aec7b0ee8fa555f258b7ac04fe607b2abc4ab305
danio86 <>
2023-07-29 12:39:51 +0000

    startet create Propertiy form

---
+++ src/components/common/PropertyForm.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import CreateProperties from '../../pages/CreateProperties';
+import { FormProps } from 'react-hook-form';
+import { Card, CloseButton, Container } from 'react-bootstrap';
+import { CommonButtonProps } from 'react-bootstrap/esm/Button';
+import CustomButton from './CustomButton'; 
+// import { Form } from 'react-hook-form';
+
+const PropertyForm = ({title}) => {
+  return (
+        
+            <div>{title}</div>
+        
+       
+  )
+}
+
+export default PropertyForm
--- src/pages/CreateProperties.js
+++ src/pages/CreateProperties.js
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useGetIdentity } from 'react-admin';
+// import reactRouterDom from 'react-router-dom';
+import { NavLink, Form, Card, Container } from 'react-bootstrap';
+import { useForm } from "react-hook-form";
+import PropertyForm from '../components/common/PropertyForm';
+// import styles from "../styles/PropertyForm.module.css";
+import styles from "../styles/PropertiesForm.module.css";
+// import styles from "../styles/PieChart.module.css";
+
+
+
 
 const CreateProperties = () => {
+    const navi = (
+        <NavLink
+        //   className={styles.NavLink}
+        //   activeClassName={styles.Active}
+          to="/posts/create">
+        </NavLink> );
+    // const {data: user} = useGetIdentity();
+    // const [data, user] = useGetIdentity();
+    const [data, setData] = useState({ hits: [] });
+    const [propertyImage, setPropertyImage] = useState({name: '', url: ''})
+    // const { for: {onFinish, formLoading}, register, handleSubmit } = useForm();
+    // const { register, handleSubmit } = useForm();
+    // const useForm =
+    const handleImageChange = () => {}
+    const onFinishHandler = () => {}
+
+
   return (
-    <div>CreateProperties</div>
+    
+
+    <Container className={styles.Form}>
+        <Card>
+            <PropertyForm
+                title={'Property Form'}
+                value={52}
+                
+            />
+        </Card>
+    </Container>
+  
+
   )
 }
 
